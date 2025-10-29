9c5911dc8d9b92672cce9639c1a8e4f9caa06bfa
Dusk-wings <pantkaran05@gmail.com>
2025-10-30 01:03:38 +0530

    feat: Add logout functionality and UI components for account management

--- frontend/src/app/account/page.tsx
+++ frontend/src/app/account/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+import Spinner from "@/components/spinner/Spinner";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -5,9 +8,33 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import React from "react";
+import { supabaseBrowser } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 function Account() {
+  const router = useRouter();
+
+  const [isProcessing, setIsProcessing] = useState(false);
+  const handelLogOut = async () => {
+    setIsProcessing(true);
+    try {
+      await supabaseBrowser.auth.signOut();
+
+      const response = await fetch(
+        new URL("/api/auth/logout", process.env.NEXT_PUBLIC_SERVER_PATH),
+        { method: "POST" }
+      );
+      if (!response.ok) throw new Error("Server logout failed");
+
+      router.replace("/authentication");
+    } catch (error) {
+      console.error("Logout failed:", error);
+    } finally {
+      setIsProcessing(false);
+    }
+  };
+
   return (
     <Card>
       <CardHeader>
@@ -16,7 +43,21 @@ function Account() {
           Your account user details are stated below
         </CardDescription>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent>
+        <div id="danger-button" className="flex flex-col gap-2">
+          <Button
+            type="button"
+            className="hover:bg-red-900 bg-red-800/70 cursor-pointer text-white font-bold"
+            onClick={handelLogOut}
+            disabled={isProcessing}
+          >
+            {isProcessing ? <Spinner text={"Logging Out"} /> : "Logout"}
+          </Button>
+          <Button variant={"destructive"} type="button" disabled={isProcessing}>
+            Delete Account
+          </Button>
+        </div>
+      </CardContent>
     </Card>
   );
 }
--- frontend/src/app/authentication/page.tsx
+++ frontend/src/app/authentication/page.tsx
@@ -1,11 +1,12 @@
 import AuthenticationFrom from "@/components/authentication-form";
 import FormEnclosure from "@/components/containers/FormEnclosure";
+import AuthValidator from "@/components/validateAuthentication/Validate";
 import React from "react";
 
 async function Authentication() {
-  
   return (
     <FormEnclosure>
+      <AuthValidator />
       <AuthenticationFrom className="lg:w-2/6 md:w-1/2 max-[520px]:w-11/12 w-8/12 gap-6" />
     </FormEnclosure>
   );
